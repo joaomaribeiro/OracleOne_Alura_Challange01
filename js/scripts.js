25711e17d8e78b471c966c40ea284cc469ca2331
João Marcelo <joaomaribeiro@gmail.com>
2022-12-26 17:31:05 -0300

    Açoes de mostrar texto e click no botão funcionando, 50% pronto

--- js/scripts.js
+++ js/scripts.js
@@ -6,7 +6,47 @@ dateTarget.innerHTML = date;
 
 const btnDecoder = document.getElementById ("clickDecoder")
 var element = document.getElementById("encoder");
-btnDecoder.onclick = function () {
-	
-	element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+btnDecoder.onclick = bubble;
+
+
+var n = 0;
+var intervalID = null;
+var element = document.getElementById('dump');
+var content = element.innerText;
+const splits = new RegExp('\n');
+var item = content.split(splits);
+var linhas  = item.length;
+element.innerHTML = "";
+var programa;
+
+
+function escreve () {
+  	element.insertAdjacentHTML('beforeEnd',(item[n]+"<br>"));
+    element.scrollIntoView({ behavior: 'smooth', block: 'end'});
+}
+
+
+
+function loop() {
+    var rand = Math.round(Math.random()*100);
+    var i = setTimeout(function() {
+    	if (n++ < linhas-1) {
+    		n++;
+            escreve();
+            loop();  
+        } else {
+        	clearTimeout(i);
+        	var jump = document.getElementById(programa);
+        	jump.scrollIntoView({ behavior: 'smooth', block: 'end'});
+        	console.log(programa)
+        }
+    }, rand);
+}
+
+function bubble (event) {
+	element.innerHTML = "";
+	n=0;
+	console.log (event.target.getAttribute('name'));
+	programa= event.target.getAttribute('name')
+	loop();
 }
