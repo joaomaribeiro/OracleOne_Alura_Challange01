45905b677f1813e421e8681e8e600885a9908138
João Marcelo <joaomaribeiro@gmail.com>
2022-12-27 15:26:58 -0300

    decodificador abrindo e funcionando, falta somente estilos

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,11 @@ console.log(date); // Fri Jun 17 2022 11:27:28 GMT+0100 (British Summer Time)
 const dateTarget = document.getElementById ("data");
 dateTarget.innerHTML = date;
 
+const MenuEncoder = document.getElementById ("clickEncoder")
 const MenuDecoder = document.getElementById ("clickDecoder")
 var element = document.getElementById("encoder");
 MenuDecoder.onclick = bubble;
+MenuEncoder.onclick = bubble;
 
 
 var n = 0;
@@ -19,7 +21,7 @@ element.innerHTML = "";
 var programa;
 var scrollTempo= 300;
 var programaSelecao;
-
+var a;
 
 function escreve () {
   	element.insertAdjacentHTML('beforeEnd',(item[n]+"<br>"));
@@ -57,15 +59,19 @@ var index = 0;
 var button1 = document.getElementById('BtnCodificar');
 var textoFinal = document.getElementById("TextoCodificadoFinal");
 var alvoRespostaCodigo = document.getElementsByClassName("AlvoRespostaCodigo");
-var copiarWrap = document.getElementsByClassName("copiar invisible");
+var copiarWrap = document.getElementById("copiarCodificarWrap");
 
 button1.onclick = function (){
 	//pega o valor do input como variavel
-	let str = document.getElementById("TextoCodificador").value; 
+	let textoOriginal = document.getElementById("TextoCodificador");
+	let str = textoOriginal.value; 
+	let cleanStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+	textoOriginal.value = cleanStr;
+
 	index = 0;
 
 	//inicializa uma variavel local, troca com regex a letra e por enter, case insensitive
-	let res = str.replace(/e/gim, "enter");
+	let res = cleanStr.replace(/e/gim, "enter");
 	//faz o replace da string utilizando regex. Flags gim = global, case insensitive, multiline
 	res = res.replace(/i/gim, "imes");
 	res = res.replace(/a/gim, "ai");
@@ -73,12 +79,46 @@ button1.onclick = function (){
 	res = res.replace(/u/gim, "ufat");
 	//escreve na página o resultado da string criptografada
 	textoFinal.setAttribute("class", "visible textofinal");
-	copiarWrap[index].setAttribute("class","visible copiar");
+	copiarWrap.setAttribute("class","visible copiar");
 	alvoRespostaCodigo[index].value = res;
 
 }
+
+// programa decodificar
+var button2 = document.getElementById('BtnDeCodificar');
+var textoFinal2 = document.getElementById("TextoDeCodificadoFinal");
+var alvoRespostaCodigo = document.getElementsByClassName("AlvoRespostaCodigo");
+var copiarWrap2 = document.getElementById("copiarDeCodificarWrap");
+
 var btnCopiar = document.getElementById("BtnCodificarCopiar");
+var btnCopiar2 = document.getElementById("BtnDeCodificarCopiar")
 btnCopiar.onclick = copiar;
+btnCopiar2.onclick = copiar;
+
+button2.onclick = function (){
+	//pega o valor do input como variavel
+	let textoOriginal = document.getElementById("TextoDeCodificador");
+	let str = textoOriginal.value; 
+	let cleanStr = str.normalize('NFD').replace(/[\u0300-\u036f]/g, "").toLowerCase();
+	textoOriginal.value = cleanStr;
+
+	index = 1;
+
+	
+	//faz o replace da string utilizando regex. Flags gim = global, case insensitive, multiline
+	let res2 = cleanStr.replace(/enter/gim, "e");
+	res2 = res2.replace(/imes/gim, "i");
+	res2 = res2.replace(/ai/gim, "a");
+	res2 = res2.replace(/ober/gim, "o");
+	res2 = res2.replace(/ufat/gim, "u");
+	//escreve na página o resultado da string criptografada
+	textoFinal2.setAttribute("class", "visible textofinal");
+	copiarWrap2.setAttribute("class","visible copiar");
+	alvoRespostaCodigo[index].value = res2;
+
+}
+
+
 function copiar() {
   // Get the text field
   
@@ -93,8 +133,14 @@ function copiar() {
   // Alert the copied text
   
 }
+
+
+
+
 buttonFecharCodificar = document.getElementById("BtnFecharCodificador");
+buttonFecharDeCodificar = document.getElementById("BtnFecharDeCodificador");
 buttonFecharCodificar.onclick = fecharPrograma;
+buttonFecharDeCodificar.onclick = fecharPrograma;
 
 function fecharPrograma () {
 	element.innerHTML = "";
@@ -102,6 +148,7 @@ function fecharPrograma () {
 	textoFinal.value="";
 	textoFinal.setAttribute("class", "visible textofinal");
 	programaSelecao.setAttribute("class","invisible");
-	document.getElementsByClassName("visible copiar")[index].setAttribute("class","copiar invisible");
-	document.getElementById("TextoCodificador").value = "";
+	a = document.getElementsByClassName("visible copiar");
+	a[0].setAttribute("class","copiar invisible");
+	document.getElementsByClassName("ExchangeArea")[index].value = "";
 }
